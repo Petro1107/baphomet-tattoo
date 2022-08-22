12b1ecc61b185c6132b5fe639d82a41420a1f7b5
Lucas Pietrovito <andypetrova@gmail.com>
2022-08-22 09:50:42 -0300

    Primer commit. Banner a ser reemplazado por parallax

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavigationBar from './components/NavigationBar';
+import Banner from './components/Banner';
+
+// EMPEZAR ESTILO MOBILE FIRST
+
+function App() {
+  return (
+    <>
+      <Banner />
+      <NavigationBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+
+import '../styles/Banner.css';
+
+// EMPEZAR ESTILO MOBILE FIRST
+
+function Banner() {
+  return (
+    <div className='banner-container'>
+      <img src='https://cuarteldelmetal.com/wp-content/uploads/2022/07/Baphomet.jpg' />
+      {/* <img src='' /> */}
+    </div>
+  );
+}
+
+export default Banner;
