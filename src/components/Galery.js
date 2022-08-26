34319688459b149d67cfdf6661bdac5a09a7bb05
Petro1107 <andypetrova@gmail.com>
2022-08-26 00:10:28 -0300

    Gallery Slider done, styles added to gallery but not final

--- src/components/Galery.js
+++ src/components/Galery.js
@@ -1,32 +1,96 @@
 import React from 'react';
-//import { RViewer, RViewerTrigger } from 'react-viewerjs';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleChevronLeft,
+  faCircleChevronRight,
+  faCircleXmark,
+} from '@fortawesome/free-solid-svg-icons';
+
+/* import { RViewer, RViewerTrigger } from 'react-viewerjs'; 
+RViewer genera un bug... el trigger no se ejecuta correctamente
+y al momento de clickear la imagen queda una pantalla en blanco
+con varios errores en consola.
+*/
 
 import '../styles/Galery.css';
 
 function Galery() {
   let images = [];
-  //     require('../components/images/tattoo-galery/tg1.png'),
-  //     require('../components/images/tattoo-galery/tg2.png'),
-  //     require('../components/images/tattoo-galery/tg3.png'),
-  //     require('../components/images/tattoo-galery/tg4.png'),
-  //     require('../components/images/tattoo-galery/tg5.png'),
-  //     require('../components/images/tattoo-galery/tg6.png'),
-  //     require('../components/images/tattoo-galery/tg7.png'),
-  //     require('../components/images/tattoo-galery/tg8.png'),
-  //   ];
 
   for (let i = 1; i < 14; i++) {
-    images.push(`${require(`../components/images/tattoo-galery/tg${i}.png`)}`);
+    images.push({
+      img: `${require(`../components/images/tattoo-galery/tg${i}.png`)}`,
+    });
   }
 
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = (index) => {
+    setSlideNumber(index);
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const prevSlide = () => {
+    slideNumber === 0
+      ? setSlideNumber(images.length - 1)
+      : setSlideNumber(slideNumber - 1);
+  };
+
+  const nextSlide = () => {
+    slideNumber + 1 === images.length
+      ? setSlideNumber(0)
+      : setSlideNumber(slideNumber + 1);
+  };
+
   return (
-    <div className='galery-container'>
-      <div className='galery-item'>
-        {images.map((image) => {
-          return <img src={image} className='galery-img' />;
+    <>
+      {openModal && (
+        <div className='slider-container'>
+          <FontAwesomeIcon
+            icon={faCircleXmark}
+            className='btnClose'
+            onClick={handleCloseModal}
+          />
+          <FontAwesomeIcon
+            icon={faCircleChevronLeft}
+            className='btnPrev'
+            onClick={prevSlide}
+          />
+          <FontAwesomeIcon
+            icon={faCircleChevronRight}
+            className='btnNext'
+            onClick={nextSlide}
+          />
+
+          <div className='fullScreenImg' onClick={handleCloseModal}>
+            <img src={images[slideNumber].img} alt='tattoo' />
+          </div>
+        </div>
+      )}
+
+      <div className='galery-container'>
+        {images.map((image, index) => {
+          console.log(image);
+          return (
+            // <div className='galery-item'>
+            <img
+              src={image.img}
+              alt='tattoo'
+              className='galery-img'
+              key={index}
+              onClick={() => handleOpenModal(index)}
+            />
+            //</div>
+          );
         })}
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -4,7 +4,18 @@ import '../styles/Portrait.css';
 function Portrait() {
   return (
     <div className='section'>
-      <div className='portrait-container'></div>
+      <div className='portrait-container'>
+        <p>
+          {' '}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </div>
     </div>
   );
 }
