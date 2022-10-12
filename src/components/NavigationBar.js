f44acf5a0a577c5e2d7c94e9354b4b0357882955
Petro1107 <andypetrova@gmail.com>
2022-10-12 18:27:28 -0300

    styles debuggin

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,48 +1,59 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 import '../styles/NavigationBar.css';
 import { Link } from 'react-scroll';
 
 function NavigationBar() {
   return (
     <>
-      <Navbar bg='dark' expand='md' variant='dark' sticky='top'>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='m-auto'>
+      <nav className='navbar'>
+        <ul>
+          <li>
             <Link
               to='portrait-container'
-              spy={true}
+              spy={false}
               smooth={true}
               offset={-50}
               duration={800}
             >
-              <Nav.Link className='mx-5'>Baphomet-Tattoo</Nav.Link>
+              Baphomet-Tattoo
             </Link>
+          </li>
+          <li>
             <Link
               to='galery-container'
-              spy={true}
+              spy={false}
               smooth={true}
-              offset={-210}
+              offset={-220}
               duration={800}
             >
-              <Nav.Link className='mx-5'>Galería</Nav.Link>
+              Galería
             </Link>
-            {/* formContainer */}
+          </li>
+          {/* formContainer */}
+          <li>
             <Link
               to='formContainer'
-              spy={true}
+              spy={false}
+              smooth={true}
+              offset={-50}
+              duration={800}
+            >
+              Precios
+            </Link>
+          </li>
+          <li>
+            <Link
+              to='contactSection'
+              spy={false}
               smooth={true}
               offset={0}
               duration={800}
             >
-              <Nav.Link className='mx-5'>Precios</Nav.Link>
+              Contacto
             </Link>
-            <Nav.Link className='mx-5'>Contacto</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
